7cb30f8faca34600f87948ff6687c1360ba90477
Sergey Rakov <rako-serge@yandex.ru>
2021-09-01 11:55:00 +0300

    add layout with Data Binding

--- app/src/main/java/ru/axout/mvvm_colorguide/view/FlowersActivity.java
+++ app/src/main/java/ru/axout/mvvm_colorguide/view/FlowersActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import ru.axout.mvvm_colorguide.R;
 
-public class FlowerActivity extends AppCompatActivity {
+public class FlowersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_flowers);
     }
 }
