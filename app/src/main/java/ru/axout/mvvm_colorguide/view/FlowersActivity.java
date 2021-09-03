13e6f1edb5f6021d98179fc2d76b2bb85a7f75d3
Sergey Rakov <rako-serge@yandex.ru>
2021-09-03 23:02:30 +0300

    add adapter

--- app/src/main/java/ru/axout/mvvm_colorguide/view/FlowersActivity.java
+++ app/src/main/java/ru/axout/mvvm_colorguide/view/FlowersActivity.java
@@ -2,13 +2,24 @@ package ru.axout.mvvm_colorguide.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import ru.axout.mvvm_colorguide.R;
+import ru.axout.mvvm_colorguide.databinding.ActivityFlowersBinding;
 
 public class FlowersActivity extends AppCompatActivity {
+
+    private ActivityFlowersBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flowers);
+
+        // связываем объект класса Binding с макетом
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_flowers);
+        // теперь можно обращаться к макету через объект binding
+        binding.flowersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
--- app/src/main/java/ru/axout/mvvm_colorguide/view/FlowersAdapter.java
+++ app/src/main/java/ru/axout/mvvm_colorguide/view/FlowersAdapter.java
@@ -1,4 +1,51 @@
 package ru.axout.mvvm_colorguide.view;
 
-public class FlowersAdapter {
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+import ru.axout.mvvm_colorguide.R;
+import ru.axout.mvvm_colorguide.databinding.ItemFlowersBinding;
+import ru.axout.mvvm_colorguide.model.Flowers;
+import ru.axout.mvvm_colorguide.viewmodel.FlowersViewModel;
+
+import java.util.List;
+
+public class FlowersAdapter extends RecyclerView.Adapter<FlowersAdapter.FlowersViewHolder> {
+
+    private List<Flowers> flowers;
+
+    public FlowersAdapter(List<Flowers> flowers) {
+        this.flowers = flowers;
+    }
+
+    @NonNull
+    @Override
+    public FlowersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemFlowersBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
+                R.layout.item_flowers, parent, false);
+        return new FlowersViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FlowersViewHolder holder, int position) {
+        ItemFlowersBinding binding = holder.binding;
+        binding.setItem(new FlowersViewModel(flowers.get(position)));
+    }
+
+    @Override
+    public int getItemCount() {
+        return flowers.size();
+    }
+
+    public class FlowersViewHolder extends RecyclerView.ViewHolder {
+
+        ItemFlowersBinding binding;
+
+        public FlowersViewHolder(@NonNull ItemFlowersBinding binding) {
+            super(binding.flowerItem);
+            this.binding = binding;
+        }
+    }
 }
