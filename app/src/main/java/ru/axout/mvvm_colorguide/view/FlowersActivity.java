8834e46baa0e1c8145fb08d7a85933968ad941b1
Sergey Rakov <rako-serge@yandex.ru>
2021-09-03 23:39:57 +0300

    add a call to data

--- app/src/main/java/ru/axout/mvvm_colorguide/view/FlowersActivity.java
+++ app/src/main/java/ru/axout/mvvm_colorguide/view/FlowersActivity.java
@@ -1,15 +1,19 @@
 package ru.axout.mvvm_colorguide.view;
 
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import ru.axout.mvvm_colorguide.R;
 import ru.axout.mvvm_colorguide.databinding.ActivityFlowersBinding;
+import ru.axout.mvvm_colorguide.network.Call;
+import ru.axout.mvvm_colorguide.network.FlowerResponse;
 
 public class FlowersActivity extends AppCompatActivity {
 
     private ActivityFlowersBinding binding;
+    private Call call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,18 @@ public class FlowersActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_flowers);
         // теперь можно обращаться к макету через объект binding
         binding.flowersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        call = new Call(this, new Call.Delegate() {
+            @Override
+            public void onSuccess(FlowerResponse flowerResponse) {
+                binding.flowersRecyclerView.setAdapter(new FlowersAdapter(flowerResponse.listFlowers));
+            }
+
+            @Override
+            public void onFailure(Object t) {
+                Toast.makeText(getApplicationContext(), "Error = " + t.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+        call.execute();
     }
 }
